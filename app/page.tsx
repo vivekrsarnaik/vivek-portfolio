e280cdf7f194fd117c3b30a511abb17020fcc8f1
vivekrsarnaik <vivekrsarnaik@gmail.com>
2026-06-12 07:04:18 -0500

    improved

--- app/page.tsx
+++ app/page.tsx
@@ -9,81 +9,120 @@ import { SiGmail } from "react-icons/si";
 
 
 export default function Home() {
+  const [hovered, setHovered] = useState(false);
 
   const [hideIcons, setHideIcons] = useState(false);
+  const [activeSection, setActiveSection] = useState("home");
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
+  const sections = [
+    "home",
+    "about",
+    "experience",
+    "skills",
+    "education",
+    "projects",
+    "contact",
+  ];
+
   const handleScroll = () => {
-    const contact = document.getElementById("contact");
+  const scrollPosition = window.scrollY + 200;
 
-    if (!contact) return;
+  if (
+  window.innerHeight + window.scrollY >=
+  document.documentElement.scrollHeight - 50
+) {
+  setActiveSection("contact");
+  return;
+}
 
-    const contactTop =
-      contact.getBoundingClientRect().top;
+    sections.forEach((section) => {
+      const element = document.getElementById(section);
 
-    setHideIcons(contactTop < 300);
+      if (
+        element &&
+        scrollPosition >= element.offsetTop &&
+        scrollPosition < element.offsetTop + element.offsetHeight
+      ) {
+        setActiveSection(section);
+      }
+    });
   };
 
-  handleScroll();
   window.addEventListener("scroll", handleScroll);
+  
 
-  return () => {
-    window.removeEventListener("scroll", handleScroll);
-  };
+  return () => window.removeEventListener("scroll", handleScroll);
 }, []);
+const navLink = (section) =>
+  `px-4 py-2 rounded-full transition-all duration-300 ${
+    activeSection === section
+      ? "bg-blue-600 text-white shadow-lg"
+      : "hover:bg-blue-600/30 hover:text-white"
+  }`;
 
   return (
-     <main className="min-h-screen bg-white text-black dark:bg-black dark:text-white transition-colors duration-300">
+     <main className="relative min-h-screen bg-white text-black dark:bg-black dark:text-white transition-colors duration-300">
+      <div className="absolute inset-0 -z-10 overflow-hidden">
+
+  <div className="
+    absolute top-20 left-1/2 -translate-x-1/2
+    w-[600px] h-[600px]
+    rounded-full blur-[120px]
+
+    bg-blue-500/5
+    dark:bg-blue-500/10
+  " />
+
+</div>
       {/* Navbar */}
       <nav className="sticky top-0 z-50 bg-white/80 dark:bg-black/80 backdrop-blur-md border-b border-gray-300 dark:border-gray-800 transition-colors duration-300">
         <div className="max-w-6xl mx-auto flex justify-between items-center px-8 py-6">
-          <h1 className="text-2xl font-bold">
-            Vivek<span className="text-blue-500">.dev</span>
-          </h1>
+      <h1
+  className={`text-lg md:text-xl font-bold cursor-pointer transition-all duration-300 ${
+    hovered ? "text-blue-500" : ""
+  }`}
+  onMouseEnter={() => setHovered(true)}
+  onMouseLeave={() => setHovered(false)}
+>
+  {hovered ? "Always improving." : "Build. Learn. Repeat."}
+  <span className="animate-pulse text-blue-500 ml-1">|</span>
+</h1>
 
-          <div className="flex items-center gap-6">
-            <a
-              href="#home"
-              className="text-gray-600 dark:text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white transition"
-            >
-              Home
-            </a>
+          <div className="flex items-center gap-2">
 
-            <a
-              href="#about"
-              className="text-gray-600 dark:text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white transition"
-            >
-              About
-            </a>
-            <a
-              href="#experience"
-              className="text-gray-600 dark:text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white transition"
-            >
-              Experience
-            </a>
-            <a
-              href="#skills"
-              className="text-gray-600 dark:text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white transition"
-            >
-              Skills
-            </a>
+  <a href="#home" className={navLink("home")}>
+  Home
+</a>
 
-            <a
-              href="#projects"
-              className="text-gray-600 dark:text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white transition"
-            >
-              Projects
-            </a>
+  <a href="#about" className={navLink("about")}>
+  About
+</a>
 
-            <a
-              href="#contact"
-              className="text-gray-600 dark:text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white transition"
-            >
-              Contact
-            </a>
+  <a href="#experience" className={navLink("experience")}>
+  Experience
+</a>
 
-            <ThemeToggle />
-          </div>
+  <a href="#skills" className={navLink("skills")}>
+  Skills
+</a>
+
+  <a href="#education" className={navLink("education")}>
+  Education
+</a>
+
+  <a href="#projects" className={navLink("projects")}>
+  Projects
+</a>
+
+  <a href="#contact" className={navLink("contact")}>
+  Contact
+</a>
+
+  <ThemeToggle />
+
+</div>
         </div>
       </nav>
   <div
@@ -116,92 +155,102 @@ export default function Home() {
 </div>
       {/* Hero Section */}
       
-      <section
-        id="home"
-        className="flex items-center justify-center min-h-[80vh]"
-      >
-        <div className="text-center px-6">
-        
-          <h1 className="text-5xl md:text-7xl font-bold mb-4">
-            <motion.img
-  src="/profile.jpg"
-  alt="Vivek Sarnaik"
-  className="w-36 h-36 md:w-44 md:h-44 rounded-full object-cover mx-auto mb-8 border-4 border-blue-500 shadow-xl"
-  animate={{
-    y: [0, -8, 0],
-  }}
-  transition={{
-    duration: 3,
-    repeat: Infinity,
-  }}
-/>
-  <TypeAnimation
-    sequence={[
-      "Hi, I'm Vivek Sarnaik",
-      2000,
-      "Software Engineer",
-      2000,
-      "Full-Stack Developer",
-      2000,
-      "Cloud & Distributed Systems Engineer",
-      2000,
-    ]}
-    wrapper="span"
-    speed={50}
-    repeat={Infinity}
-  />
-</h1>
-
-          <h2 className="text-xl md:text-2xl text-gray-500 dark:text-gray-500 dark:text-gray-400 mb-6">
-            Software Engineer | Full-Stack Development | Cloud & Distributed Systems
-          </h2>
-
-          <p className="max-w-2xl mx-auto text-gray-700 dark:text-gray-300 text-lg mb-8">
-            M.S. Computer Science graduate passionate about backend engineering,
-            distributed systems, cloud technologies, and AI-powered applications.
-          </p>
-
-          <div className="flex flex-col sm:flex-row gap-4 justify-center">
-           
-            <a
-  href="#projects"
-  className="bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg font-semibold"
+     <section
+  id="home"
+  className="min-h-[85vh] flex flex-col justify-center items-center text-center px-6"
 >
-  View Projects
+
+  <motion.img
+    src="/profile.jpg"
+    alt="Vivek Sarnaik"
+    className="w-32 h-32 md:w-36 md:h-36 rounded-full object-cover border-4 border-blue-500 shadow-xl mb-6"
+    animate={{ y: [0, -8, 0] }}
+    transition={{
+      duration: 3,
+      repeat: Infinity,
+    }}
+  />
+
+  <h1 className="text-5xl md:text-7xl font-bold mb-4">
+    Hi, I'm Vivek Sarnaik
+  </h1>
+
+  <TypeAnimation
+  sequence={[
+    "Software Engineer",
+    2000,
+    "Backend Engineer",
+    2000,
+    "Cloud Engineer",
+    2000,
+    "Distributed Systems Enthusiast",
+    2000,
+    "Building Scalable Applications",
+    2000,
+  ]}
+  wrapper="span"
+  speed={50}
+  repeat={Infinity}
+  className="text-xl md:text-3xl text-gray-400 mb-6 block"
+/>
+
+  <p className="max-w-3xl text-lg text-gray-400 leading-relaxed">
+    I recently completed my Master's degree and have a strong interest in backend
+    development, distributed systems, cloud technologies, and AI-powered software.
+    I enjoy building scalable applications, solving challenging problems, and
+    creating systems that are reliable and efficient.
+  </p>
+
+  <div className="flex flex-wrap justify-center gap-4 mt-10">
+
+    <a
+      href="#projects"
+      className="bg-gradient-to-r from-blue-500 to-cyan-500 hover:from-blue-600 hover:to-cyan-600 text-white px-6 py-3 rounded-lg font-semibold transition-all duration-300"
+>
+    
+      View Projects
+    </a>
+
+    <a
+  href="/Vivek_Sarnaik_Resume.pdf"
+  download
+  className="bg-gradient-to-r from-blue-500 to-cyan-500 hover:from-blue-600 hover:to-cyan-600 text-white px-6 py-3 rounded-lg font-semibold transition-all duration-300"
+>
+  Download Resume
 </a>
 
-            <a
-            href="/Vivek_Sarnaik_Resume.pdf"
-            download
-            className="border border-white hover:bg-white hover:text-black px-6 py-3 rounded-lg font-semibold"
-            >
-             Download Resume
-            </a>
-          </div>
-        </div>
-      </section>
+  </div>
+
+</section>
       <div className="max-w-4xl mx-auto border-b border-gray-800"></div>
-      {/* About Section */}
-      <section id="about" className="py-24 px-6">
-        <div className="max-w-4xl mx-auto">
-          <h2 className="text-4xl font-bold mb-8 text-center">
-            About Me
-          </h2>
+      
+{/* About Section */}
+<section id="about" className="py-24 px-6">
 
-          <p className="text-gray-700 dark:text-gray-300 text-lg leading-8 text-center">
-            Hi, I'm Vivek, a Software Engineer who enjoys turning ideas into real applications.
+  <motion.div
+    initial={{ opacity: 0, y: 40 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5 }}
+    viewport={{ once: true }}
+    className="max-w-4xl mx-auto"
+  >
 
-I recently completed my Master's degree and have a strong interest in backend development, distributed systems, cloud technologies, and AI-powered software. I enjoy building things from scratch, whether it's a real-time chat application, a distributed job scheduler, or an AI-powered tool.
+    <h2 className="text-4xl font-bold mb-8 text-center">
+      About Me
+    </h2>
 
-My favorite part of software engineering is solving challenging problems and designing systems that can scale. I'm always learning, building, and looking for ways to improve my skills while creating meaningful projects.
-          </p>
+    <p className="max-w-3xl mx-auto text-lg text-gray-700 dark:text-gray-300 leading-relaxed text-center">
+      Hi, I'm Vivek, a Software Engineer who enjoys turning ideas into real applications.
+      I recently completed my Master's degree and have a strong interest in backend
+      development, distributed systems, cloud technologies, and AI-powered software.
+      From building real-time chat platforms and distributed job schedulers to AI-powered
+      applications, I enjoy solving complex problems and creating systems that are scalable,
+      reliable, and easy to use.
+    </p>
 
-          <p className="text-gray-700 dark:text-gray-300 text-lg leading-8 text-center mt-6">
-            I enjoy building scalable software using Python, Java, React, AWS,
-            Docker, and modern development practices.
-          </p>
-        </div>
-      </section>
+  </motion.div>
+
+</section>
       <div className="max-w-4xl mx-auto border-b border-gray-800"></div>
 
       {/* Experience Section */}
@@ -460,6 +509,63 @@ My favorite part of software engineering is solving challenging problems and des
           </div>
         </div>
       </section>
+      <div className="max-w-4xl mx-auto border-b border-gray-300 dark:border-gray-800"></div>
+      {/* Education Section */}
+<section id="education" className="py-24 px-6">
+  <div className="max-w-5xl mx-auto">
+
+    <h2 className="text-4xl font-bold text-center mb-12">
+      Education
+    </h2>
+
+    <div className="space-y-6">
+
+      <div className="border border-gray-300 dark:border-gray-800 rounded-xl p-6">
+        <h3 className="text-2xl font-semibold">
+          M.S. Information Science
+        </h3>
+
+        <p className="text-blue-500">
+          Trine University • 2026
+        </p>
+
+        <p className="text-gray-400 mt-2">
+          Cloud Computing, Advanced Databases, Software Engineering
+        </p>
+      </div>
+
+      <div className="border border-gray-300 dark:border-gray-800 rounded-xl p-6">
+        <h3 className="text-2xl font-semibold">
+          M.S. Computer Science
+        </h3>
+
+        <p className="text-blue-500">
+          University of Texas at Arlington • 2024
+        </p>
+
+        <p className="text-gray-400 mt-2">
+          Distributed Systems, Machine Learning, Algorithms, Databases
+        </p>
+      </div>
+
+      <div className="border border-gray-300 dark:border-gray-800 rounded-xl p-6">
+        <h3 className="text-2xl font-semibold">
+          B.E. Computer Engineering
+        </h3>
+
+        <p className="text-blue-500">
+          Savitribai Phule Pune University • 2021
+        </p>
+
+        <p className="text-gray-400 mt-2">
+          Computer Networks, Data Structures, Operating Systems
+        </p>
+      </div>
+
+    </div>
+
+  </div>
+</section>
       
       <div className="max-w-4xl mx-auto border-b border-gray-300 dark:border-gray-800"></div>
 
@@ -470,33 +576,42 @@ My favorite part of software engineering is solving challenging problems and des
     <h2 className="text-4xl font-bold text-center mb-12">
       Projects
     </h2>
-<motion.div
-  className="text-center mb-8"
->
-  <span className="inline-flex items-center gap-2 px-5 py-3 bg-blue-600 rounded-full text-sm font-semibold shadow-lg">
+<motion.div className="text-center mb-8">
+  <a
+    href="https://github.com/vivekrsarnaik/PetPulse-App"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="
+      inline-flex items-center gap-3
+      px-5 py-3
+      rounded-full
+      bg-blue-600
+      hover:bg-blue-700
+      text-white
+      font-medium
+      shadow-lg
+      transition-all duration-300
+    "
+  >
     <motion.span
       animate={{ rotate: [0, -20, 20, -20, 0] }}
       transition={{
         duration: 1,
         repeat: Infinity,
-        repeatDelay: 1,
+        repeatDelay: 2,
       }}
     >
       🔨
     </motion.span>
-    
 
-    
-      <a
-  href="https://github.com/vivekrsarnaik/PetPulse-App"
-  target="_blank"
-  rel="noopener noreferrer"
-  className="inline-flex items-center gap-2 text-white-500 hover:text-white-400 font-medium"
->
-  
-  Currently Building: (iOS Pet Health App) PetPulse →
-</a>
-  </span>
+    Currently Building: PetPulse
+
+    <FaGithub className="text-lg" />
+
+    <span className="transition-transform duration-300 group-hover:translate-x-1">
+      →
+    </span>
+  </a>
 </motion.div>
     <div className="grid md:grid-cols-3 gap-8">
 
@@ -546,13 +661,19 @@ My favorite part of software engineering is solving challenging problems and des
 
         <div className="mt-6">
           <a
-            href="https://github.com/vivekrsarnaik/gRPC-chat"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg font-semibold"
-          >
-            View on GitHub
-          </a>
+  href="https://github.com/vivekrsarnaik/gRPC-chat"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="group inline-flex items-center gap-2 bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg font-semibold transition-all duration-300"
+>
+  <FaGithub />
+
+  View on GitHub
+
+  <span className="transition-transform duration-300 group-hover:translate-x-1">
+    →
+  </span>
+</a>
         </div>
       </motion.div>
 
@@ -596,28 +717,63 @@ My favorite part of software engineering is solving challenging problems and des
     <li>• Docker Deployment</li>
   </ul>
   <div className="mt-6">
-  <a
-    href="https://github.com/vivekrsarnaik/distributed-job-scheduler"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="inline-block bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg font-semibold"
-  >
-    View on GitHub
-  </a>
+    <a
+  href="https://github.com/vivekrsarnaik/distributed-job-scheduler"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="group inline-flex items-center gap-2 bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg font-semibold transition-all duration-300"
+>
+  <FaGithub />
+
+  View on GitHub
+
+  <span className="transition-transform duration-300 group-hover:translate-x-1">
+    →
+  </span>
+</a>
 </div>
 </motion.div>
 
 
 
 <motion.div
-  initial={{ opacity: 0, x: 80 }}
-  whileInView={{ opacity: 1, x: 0 }}
-  transition={{ duration: 0.7 }}
-  viewport={{ once: true }}
   whileHover={{ y: -8 }}
-  className="border border-gray-300 dark:border-gray-800 rounded-xl p-6 shadow-lg"
+  className="
+    border border-gray-300 dark:border-gray-800
+    rounded-3xl p-8
+    hover:border-blue-500
+    hover:shadow-[0_0_25px_rgba(37,99,235,0.25)]
+    transition-all duration-300
+  "
 >
-  <h3 className="text-2xl font-semibold mb-4">
+ <div
+  className="relative group cursor-pointer mb-6"
+  onClick={() => setSelectedImage("/projects/ai-review.png")}
+>
+  <img
+    src="/projects/ai-review.png"
+    alt="AI Code Review Assistant"
+    className="w-full h-44 object-cover rounded-xl"
+  />
+
+  <div
+    className="
+      absolute inset-0
+      bg-black/60
+      opacity-0
+      group-hover:opacity-100
+      transition-all duration-300
+      flex items-center justify-center
+      rounded-lg
+      
+    "
+  >
+    <span className="text-white font-medium">
+      🔍 View Screenshot
+    </span>
+  </div>
+</div>
+  <h3 className="text-3xl font-bold mb-4">
     AI Code Review Assistant
 </h3>
 
@@ -648,14 +804,20 @@ My favorite part of software engineering is solving challenging problems and des
     <li>• Review History</li>
   </ul>
   <div className="mt-6">
-  <a
-    href="https://github.com/vivekrsarnaik/ai-code-review-assistant"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="inline-block bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg font-semibold"
-  >
-    View on GitHub
-  </a>
+   <a
+  href="https://github.com/vivekrsarnaik/ai-code-review-assistant"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="group inline-flex items-center gap-2 bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg font-semibold transition-all duration-300"
+>
+  <FaGithub />
+
+  View on GitHub
+
+  <span className="transition-transform duration-300 group-hover:translate-x-1">
+    →
+  </span>
+</a> 
 </div>
 </motion.div>
 
@@ -666,7 +828,7 @@ My favorite part of software engineering is solving challenging problems and des
 <div className="max-w-4xl mx-auto my-10 border-b border-gray-300 dark:border-gray-800"></div>
 
 {/* Contact Section */}
-<section id="contact" className="pt-4 pb-24 px-6">
+<section id="contact" className="pt-16 pb-12 px-6">
   <div className="max-w-4xl mx-auto text-center">
 
     <h2 className="text-4xl font-bold mb-8">
@@ -715,6 +877,34 @@ My favorite part of software engineering is solving challenging problems and des
 
   </div>
 </section>
+{selectedImage && (
+  <div
+    className="fixed inset-0 bg-black/80 flex items-center justify-center z-50"
+    onClick={() => setSelectedImage(null)}
+  >
+    <img
+      src={selectedImage}
+      alt="Full Screenshot"
+      className="max-w-6xl max-h-[90vh] rounded-xl shadow-2xl"
+    />
+  </div>
+)}
+{/* Footer */}
+<footer className="mt-12 border-t border-gray-300 dark:border-gray-800 py-8">
+
+  <div className="max-w-6xl mx-auto px-6 text-center">
+
+    <p className="text-gray-600 dark:text-gray-400">
+      Built with Next.js, Tailwind CSS & Framer Motion
+    </p>
+
+    <p className="text-sm text-gray-500 mt-2">
+      © 2026 Vivek Sarnaik. All rights reserved.
+    </p>
+
+  </div>
+
+</footer>
     </main>
   );
 }
